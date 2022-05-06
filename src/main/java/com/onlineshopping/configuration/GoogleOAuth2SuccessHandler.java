1d1c9ec40ee4ff6abc716f8694ffb48739e2eca7
Muhammad Asad <muhammadasadtech@gmail.com>
2022-05-06 08:07:03 +0100

    Create GoogleOAuth2SuccessHandler.java

---
+++ src/main/java/com/onlineshopping/configuration/GoogleOAuth2SuccessHandler.java
@@ -0,0 +1,62 @@
+//
+//package com.onlineshopping.configuration;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//import org.apache.http.client.RedirectStrategy;
+//import org.apache.http.impl.client.DefaultRedirectStrategy;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+//import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+//import org.springframework.stereotype.Component;
+//
+//import com.onlineshopping.model.Role;
+//import com.onlineshopping.model.User;
+//import com.onlineshopping.repository.RoleRepository;
+//import com.onlineshopping.repository.UserRepository;
+//
+//@Component
+//public abstract class GoogleOAuth2SuccessHandler implements AuthenticationSuccessHandler {
+//
+//	@Autowired
+//	RoleRepository roleRepository;
+//
+//	@Autowired
+//	UserRepository userRepository;
+//
+//	private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+//
+//	@Override
+//	public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain chain,
+//			Authentication authentication) throws IOException, ServletException {
+//		
+//		OAuth2AuthenticationToken token = (OAuth2AuthenticationToken) authentication;
+//		String email = token.getPrincipal().getAttributes().get("email").toString();
+//		if (userRepository.findUserByEmail(email).isPresent()) {
+//
+//		} else {
+//			User user = new User();
+//			user.setFirstName(token.getPrincipal().getAttributes().get("given_name").toString());
+//			user.setLastName(token.getPrincipal().getAttributes().get("family_name").toString());
+//			user.setEmail(email);
+//			List<Role> roles = new ArrayList<>();
+//			roles.add(roleRepository.findById(2).get());
+//			user.setRoles(roles);
+//			userRepository.save(user);
+//			
+//		}
+//		httpServletResponse.sendRedirect("/");
+//		return;
+//	}
+//	
+//		
+//}
+//
